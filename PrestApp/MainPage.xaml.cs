218483f4845f1377ead9b80e4890f95ef2aa3f3c
Nadia Disla <tejadanadiam@gmail.com>
2019-11-01 12:35:51 -0400

    Cambios MainPage

--- PrestApp/MainPage.xaml.cs
+++ PrestApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Android.Widget;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -19,6 +20,12 @@ namespace PrestApp
         }
         private void TxtUsuario_Focused(object sender, FocusEventArgs e)
         {
+
+        }
+
+        private void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            Toast.MakeText(MainPage.Con, "hola", ToastLength.Short).Show();
         }
     }
 }
