98f25d68025af298e2ac0fd867d170d9c8cc5c41
Jose Armando Mendez <jamendez216@gmail.com>
2019-11-06 09:59:14 -0400

    Generic... No se si sirve

---
+++ Prestapp.Data/DataAccess/Generic/GenericData.cs
@@ -0,0 +1,111 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestapp.Data.DataAccess.Generic
+{
+    public class GenericData<T> : IGenericData<T> where T : class, new()
+    {
+        private SQLiteAsyncConnection db;
+
+        public GenericData(SQLiteAsyncConnection db)
+        {
+            this.db = db;
+        }
+
+        public AsyncTableQuery<T> AsQueryable() =>
+            db.Table<T>();
+
+        public async Task<List<T>> Get() =>
+            await db.Table<T>().ToListAsync();
+
+        public async Task<List<T>> Get<TValue>(Expression<Func<T, bool>> predicate = null, Expression<Func<T, TValue>> orderBy = null)
+        {
+            try
+            {
+                var query = db.Table<T>();
+
+                if (predicate != null)
+                    query = query.Where(predicate);
+
+                if (orderBy != null)
+                    query = query.OrderBy<TValue>(orderBy);
+
+                return await query.ToListAsync();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+        }
+
+        public async Task<T> Get(int id)
+        {
+            try
+            {
+                return await db.FindAsync<T>(id);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            try
+            {
+                return await db.FindAsync<T>(predicate);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        public async Task<int> Insert(T entity)
+        {
+            try
+            {
+                return await db.InsertAsync(entity);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+        public async Task<int> Update(T entity)
+        {
+            try
+            {
+                return await db.UpdateAsync(entity);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+            
+        public async Task<int> Delete(T entity)
+        {
+            try
+            {
+                return await db.DeleteAsync(entity);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            
+        }
+    }
+}
