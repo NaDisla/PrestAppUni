45e6a959ae62d263be97c4e5783176b8c50a1f39
Jose Armando Mendez <jamendez216@gmail.com>
2019-11-08 14:37:28 -0400

    Generic y RolesController

---
+++ PrestApp.Apii/Controllers/RolesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PrestApp.Clases;
+using PrestApp.Generic;
+
+namespace PrestApp.Apii.Controllers
+{
+    [Route("api/Roles")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        [Route("Index")]
+        [HttpGet]
+        public ObjectResult ListRoles()
+        {
+            string nombreArchivo = "BD_PrestApp.sqlite";
+            string rutaCarpeta = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string rutaCompleta = Path.Combine(rutaCarpeta, nombreArchivo);
+            try
+            {
+                IGeneric<Roles> generic = new Generic<Roles>(rutaCompleta);
+                var roles = generic.Get();                
+                return Ok(roles);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+    }
+}
---
+++ PrestApp/Generic/Generic.cs
@@ -0,0 +1,55 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestApp.Generic
+{
+    public class Generic<T> : IGeneric<T> where T : class, new()
+    {
+        protected SQLiteConnection db;
+
+        public Generic(string dbPath)
+        {
+            db = new SQLiteConnection(dbPath);
+            db.CreateTable<T>();
+        }
+
+        public TableQuery<T> AsQueryable() =>
+            db.Table<T>();
+
+        public List<T> Get() =>
+             db.Table<T>().ToList();
+
+        public List<T> Get<TValue>(Expression<Func<T, bool>> predicate = null, Expression<Func<T, TValue>> orderBy = null)
+        {
+            var query = db.Table<T>();
+
+            if (predicate != null)
+                query = query.Where(predicate);
+
+            if (orderBy != null)
+                query = query.OrderBy<TValue>(orderBy);
+
+            return query.ToList();
+        }
+
+        public T Get(int id) =>
+              db.Find<T>(id);
+
+        public T Get(Expression<Func<T, bool>> predicate) =>
+            db.Find<T>(predicate);
+
+        public int Insert(T entity) =>
+              db.Insert(entity);
+
+        public int Update(T entity) =>
+             db.Update(entity);
+
+        public int Delete(T entity) =>
+            db.Delete(entity);
+    }
+}
